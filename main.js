15528d105c9f93ef1bc378f0e88d92b945dc1583
IronLegends7519 <drafin@edenschool.fr>
2024-02-14 13:52:59 +0100

    first commit - finish design

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+let balence = document.querySelector('#balence');
+let receive = document.querySelector('#receive');
+let send = document.querySelector('#send');
+let price = balence.textContent += ".00€";
+
+receive.addEventListener('change', () => {
+    if()
+})
